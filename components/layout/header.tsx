fd537e7e216bbf9b10fbda204306f3ac390d8668
shota-nagato <shota.nagato@gmail.com>
2025-06-21 18:12:12 +0900

    headerにgithubリンク追加

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -18,18 +18,40 @@ export async function Header() {
         <Link href="/">
           <Image src="/image/logo.png" alt="logo" width={209} height={24} />
         </Link>
-        <ul
-          className={`
-            hidden gap-10
-            lg:flex
-          `}
-        >
-          {categories.map((category) => (
-            <li key={category.id} className="body text-white">
-              <Link href="#">{category.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className="flex items-center gap-10">
+          <ul
+            className={`
+              hidden gap-10
+              lg:flex
+            `}
+          >
+            {categories.map((category) => (
+              <li key={category.id} className="body text-white">
+                <Link href="#">{category.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <Link
+            href="https://github.com/shota-nagato"
+            target="_blank"
+            className="flex items-center gap-2"
+          >
+            <Image
+              src="/svg/github-white.svg"
+              alt="github"
+              width={20}
+              height={20}
+            />
+            <span
+              className={`
+                hidden text-white
+                lg:block
+              `}
+            >
+              GitHub
+            </span>
+          </Link>
+        </div>
       </div>
     </header>
   )
