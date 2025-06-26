cf0747262bbcb250429c48931f609cf0c2101a7c
shota-nagato <shota.nagato@gmail.com>
2025-06-26 20:49:59 +0900

    footerリンクを非表示

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,11 +4,11 @@ export function Footer() {
   return (
     <footer className="bg-primary-black mt-[180px]">
       <div className="flex flex-col items-center gap-6 py-8 text-white">
-        <div className="flex items-center gap-10">
+        {/* <div className="flex items-center gap-10">
           <Link href="/">お問い合わせ</Link>
           <Link href="/">プライバシーポリシー</Link>
           <Link href="/">サイトマップ</Link>
-        </div>
+        </div> */}
         <small>&copy; 2025 NagatoTech All rights reserved.</small>
       </div>
     </footer>
